b9a6031081d6e93e72296495e8109b6092fc7921
RelightSoul <ajia9ikpobb@gmail.com>
2022-06-16 20:31:59 +0300

    Разбор отличия Virtual/New

---
+++ 3.04.MethodsPropHiding/Program.cs
@@ -0,0 +1,75 @@
+﻿// Фактически скрытие метода/свойства представляет определение в классе-наследнике метода или свойства,
+// которые соответствует по имени и набору параметров методу или свойству базового класса. Для скрытия
+// членов класса применяется ключевое слово new.
+
+#region Скрытие методов
+class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;    
+    }
+    public void Print()
+    {
+        Console.WriteLine(Name);
+    }
+}
+class Employee : Person
+{
+    public string Company { get; set; }
+    public Employee(string name, string company) : base(name)
+    {
+        Company = company;      
+    }
+    public new void Print()
+    {
+        Console.WriteLine($"{Name} {Company}");
+    }
+    //  Например, в примере выше метод Print в базовом классе не является виртуальным,
+    //  мы не можем его переопределить, но, допустим, нас не устраивает его реализация
+    //  для производного класса, поэтому мы можем воспользоваться сокрытием, чтобы определить
+    //  нужный нам функционал. 
+}
+#endregion
+
+#region Скрытие свойств
+class Person2
+{
+    public string Name { get; set; }
+    public Person2(string name)
+    {
+        Name = name;    
+    }
+}
+class Employee2 : Person2
+{
+    public new string Name
+    {
+        get => $"Mr./Ms. {base.Name}"; 
+        set => base.Name = value;    
+    }
+    public string Company { get; set; }
+    public Employee2(string name, string company) : base(name)
+    {
+        Company = company;  
+    }
+
+}
+#endregion
+
+#region Скрытие переменных и констант
+//  В отличие от переопределения C# позволяет применять скрытие к переменным
+//  (как к статическим, так и нестатическим) и константам
+
+class Person3
+{
+    public const int minAge = 5;
+    public readonly string Type = "Person";
+}
+class Employee3 : Person3
+{
+    public new const int minAge = 10;
+    public new readonly string Type = "Employee";
+}
+#endregion
---
+++ 3.05.NewVirtualDifference/Program.cs
@@ -0,0 +1,74 @@
+﻿// Различие переопределения и скрытия методов
+#region Переопределение
+class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine(Name);
+    }
+}
+class Employee : Person
+{
+    public string Company { get; set; }
+    public Employee(string name, string company) : base(name)
+    {
+        Company = company;  
+    }
+    public override void Print()
+    {
+        Console.WriteLine($"{Name} работает в {Company}");
+    }
+}
+//  Используем классы в программе
+#endregion
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person tom = new Employee("Tom","BMW");
+        tom.Print();
+        //  При вызове tom.Print() выполняется реализация метода Print из класса Employee,
+        //  несмотря на то, что переменная tom - переменная типа Person.
+
+        Person2 tom2 = new Employee2("Tom","BMW");
+        tom2.Print();
+        //  Переменная tom2 представляет тип Person2, но хранит ссылку на объект Employee2.
+        //  Однако при вызове метода Print будет выполняться та версия метода, которая
+        //  определена именно в классе Person2, а не в классе Employee2. Почему? Класс Employee2
+        //  никак не переопределяет метод Print, унаследованный от базового класса, а фактически
+        //  определяет новый метод. Поэтому при вызове tom.Print() вызывается метод Print из класса Person.
+    }
+}
+
+#region Скрытие
+class Person2
+{
+    public string Name { get; set; }
+    public Person2(string name)
+    {
+        Name = name;
+    }
+    public void Print()
+    {
+        Console.WriteLine(Name);
+    }
+}
+class Employee2 : Person2
+{
+    public string Company { get; set; }
+    public Employee2(string name, string company) : base(name)
+    {
+        Company = company;
+    }
+    public new void Print()
+    {
+        Console.WriteLine($"{Name} работает в {Company}");
+    }
+}
+#endregion
