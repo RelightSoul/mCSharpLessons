2ce47e58f12f4dcbe431d59ce620a96db3e16662
RelightSoul <ajia9ikpobb@gmail.com>
2022-06-18 18:46:13 +0300

    Разбор темы делегатов

---
+++ 5.02.DelegateExamples/Program.cs
@@ -0,0 +1,118 @@
+﻿// Применение делегатов
+
+using System;
+
+public delegate void AccountHandler(string message);  //объявляем делегат
+public class Account
+{
+    int sum;
+    AccountHandler? taken;                            //Создаём переменную делегата
+    public Account(int sum) => this.sum = sum;
+    public void RegisterHandler(AccountHandler del)   //Регистрируем делегат
+    {
+        taken = del;
+    }
+    public void Add(int sum) => this.sum += sum;
+    public void Take(int sum)
+    {
+        if (this.sum >= sum)
+        {
+            this.sum -= sum;
+            taken?.Invoke($"Со счёта списано {sum} y.e"); // // вызываем делегат, передавая ему сообщение
+        }
+        else
+        {
+            taken?.Invoke($"Недостаточно средств, текущий баланс {this.sum} y.e");
+        }
+    }
+}
+//  Поскольку делегат AccountHandler в качестве параметра принимает строку, то при вызове
+//  переменной taken() мы можем передать в этот вызов конкретное сообщение. В зависимости
+//  от того, произошло снятие денег или нет, в вызов делегата передаются разные сообщения.
+
+//  То есть фактически вместо делегата будут выполняться действия, которые переданы делегату
+//  в методе RegisterHandler. Причем опять же подчеркну, при вызове делегата мы не значем,
+//  что это будут действия. Здесь мы только передаем в эти действия сообщение об успешно или
+//  неудачном снятии.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account account = new Account(200);
+        account.RegisterHandler(PrintSimpleMessage);
+        account.Take(100);
+        account.Take(150);
+        Console.WriteLine();
+
+        Account2 account2 = new Account2(200);
+        account2.RegisterHandler(PrintSimpleMessage);
+        account2.RegisterHandler(PrintColorMessage);
+        account2.Take(100);
+        account2.Take(150);
+        account2.UnregisterHandler(PrintColorMessage);
+        account.Take(50);
+
+    }
+    static void PrintSimpleMessage(string message) => Console.WriteLine(message);
+    static void PrintColorMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
+//  Здесь через метод RegisterHandler переменной taken в классе Account передается ссылка на
+//  метод PrintSimpleMessage. Этот метод соответствует делегату AccountHandler. Соответственно
+//  там, где вызывается делегат taken в методе Account, в реальности будет выполняться метод
+//  PrintSimpleMessage.
+
+//  Через параметр message метод PrintSimpleMessage получит переданное из делегата сообщение
+//  и выведет его на консоль:
+
+//  В результате, если мы создаем консольное приложение, мы можем через делегат выводить сообщение
+//  на консоль. Если мы создаем графическое приложение Windows Forms или WPF, то можно выводить
+//  сообщение в виде графического окна. А можно не просто выводить сообщение. А, например,
+//  записать при списании информацию об этом действии в файл или отправить уведомление на
+//  электронную почту. В общем любыми способами обработать вызов делегата. И способ обработки
+//  не будет зависеть от класса Account.
+
+#region Добавление и удаление методов в делегате
+//  Хотя в примере наш делегат принимал адрес на один метод, в действительности он может указывать
+//  сразу на несколько методов. Кроме того, при необходимости мы можем удалить ссылки на адреса
+//  определенных методов, чтобы они не вызывались при вызове делегата. Итак, изменим в классе
+//  Account2 метод RegisterHandler и добавим новый метод UnregisterHandler, который будет удалять
+//  методы из списка методов делегата:
+
+public class Account2
+{
+    int sum;
+    AccountHandler? taken;                            
+    public Account2(int sum) => this.sum = sum;
+    public void RegisterHandler(AccountHandler del)  
+    {
+        taken += del;
+    }
+    public void UnregisterHandler(AccountHandler del)
+    {
+        taken -= del;  // удаляем делегат
+    }
+    public void Add(int sum) => this.sum += sum;
+    public void Take(int sum)
+    {
+        if (this.sum >= sum)
+        {
+            this.sum -= sum;
+            taken?.Invoke($"Со счёта списано {sum} y.e");
+        }
+        else
+        {
+            taken?.Invoke($"Недостаточно средств, текущий баланс {this.sum} y.e");
+        }
+    }
+}
+//  В первом методе объединяет делегаты taken и del в один, который потом
+//  присваивается переменной taken. Во втором методе из переменной taken удаляется делегат del.
+
+//  Рассмотрим пример в классе Programm выше
+#endregion
---
+++ 5.04.Lambdas/Program.cs
@@ -0,0 +1,164 @@
+﻿// Лямбда-выражения представляют упрощенную запись анонимных методов.
+// Лямбда-выражения позволяют создать емкие лаконичные методы, которые
+// могут возвращать некоторое значение и которые можно передать в качестве
+// параметров в другие методы.
+
+//  Ламбда-выражения имеют следующий синтаксис: слева от лямбда-оператора =>
+//  определяется список параметров, а справа блок выражений, использующий эти параметры:
+
+//      (список_параметров) => выражение
+Message hello = () => Console.WriteLine("Hello");
+hello();
+hello();
+hello();
+//  delegate void Message();   объявлен в конце кода
+
+//  В данном случае переменная hello представляет делегат Message - то есть некоторое действие,
+//  которое ничего не возвращает и не принимает никаких параметров. В качестве значения этой
+//  переменной присваивается лямбда-выражение. Это лямбда-выражение должно соответствовать
+//  делегату Message - оно то же не принимает никаких параметров, поэтому слева от лямбда-оператора
+//  идут пустые скобки. А справа от лямбда-оператора идет выполняемое выражение -
+//  Console.WriteLine("Hello")
+
+//  Если лямбда-выражение содержит несколько действий, то они помещаются в фигурные скобки:
+Message hello2 = () =>
+{
+    Console.Write("Hello ");
+    Console.WriteLine("sweet");
+};
+hello2();
+
+//  Выше мы определили переменную hello, которая представляет делегат Message. Но начиная
+//  с версии C# 10 мы можем применять неявную типизацию (определение переменной с помощью
+//  оператора var) при определении лямбда-выражения:
+
+var hello3 = () => Console.WriteLine("Bye");
+hello3();
+
+//  Но какой тип в данном случае представляет переменная hello? При неявной типизации компилятор
+//  сам пытается сопоставить лямбда-выражение на основе его опеределения с каким-нибудь делегатом.
+//  Например, выше определенное лямбда-выражение hello по умолчанию компилятор будет рассматривать
+//  как переменную встроенного делегата Action, который не принимает никаких параметров и ничего
+//  не возвращает.
+#region Параметры лямбды
+//  При определении списка параметров мы можем не указывать для них тип данных:
+Operation sum = (x, y) => Console.WriteLine($"{x} + {y} = {x + y}");
+//  В данном случае компилятор видит, что лямбда-выражение sum представляет тип Operation,
+//  а значит оба параметра лямбды представляют тип int. Поэтому никак проблем не возникнет.
+//  Однако если мы применяем неявную типизацию, то у компилятора могут возникнуть трудности,
+//  чтобы вывести тип делегата для лямбда-выражения, например, в следующем случае
+
+//  var sum = (x, y) => Console.WriteLine($"{x} + {y} = {x + y}");   // ! Ошибка
+
+//  В этом случае можно указать тип параметров
+var sum2 = (int x, int y) => Console.WriteLine($"{x} + {y} = {x + y}");
+sum2(55,23);
+
+//  Если лямбда имеет один параметр, для которого не требуется указывать тип данных,
+//  то скобки можно опустить:
+PrintMessage mes44 = message => Console.WriteLine(message);
+mes44("Around the world");
+//  delegate void PrintMessage(string message);   объявлен в конце кода
+#endregion
+
+#region Возвращение результата
+//  Лямбда-выражение может возвращать результат. Возвращаемый результат можно указать
+//  после лямбда-оператора:
+
+var sum3 = (int x, int y) => x + y;
+int sum3Result = sum3(33, 44);
+Console.WriteLine(sum3Result);
+
+Operation2 mult = (x, y) => x * y;
+int multResult = mult(334, 566);
+Console.WriteLine(multResult);
+
+//  Если лямбда-выражение содержит несколько выражение, тогда нужно использовать оператор
+//  return, как в обычных методах:
+var subtract = (int x, int y) =>
+{
+    if (x > y) return x - y;
+    else return y - x;
+};
+Console.WriteLine();
+#endregion
+
+#region Добавление и удаление действий в лямбда-выражении
+//  Поскольку лямбда-выражение представляет делегат, тот как и в делегат, в переменную,
+//  которая представляет лямбда-выражение можно добавлять методы и другие лямбды:
+
+var _hello = () => Console.WriteLine("***** (-_-) *****");
+var _message = () => Console.Write("Hello ");
+_message += () => Console.WriteLine("World");
+_message += _hello;
+_message += Print;
+
+_message();
+Console.WriteLine("----- Удалю несколько методов -----");
+
+_message -= Print;
+_message -= _hello;
+
+_message?.Invoke();    // на случай, если в message больше нет действий
+
+void Print() => Console.WriteLine("Weclome to C#");
+Console.WriteLine();
+#endregion
+
+#region Лямбда-выражение как аргумент метода
+//  Как и делегаты, лямбда-выражения можно передавать параметрам метода, которые представляют делегат:
+int[] interers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+int result = Sum(interers, x => x > 5);   // найдем сумму чисел больше 5
+Console.WriteLine(result);
+
+int result2 = Sum(interers, x => (x % 2) == 0);  // найдем сумму четных чисел
+Console.WriteLine(result2);
+
+int Sum(int[] numbers, IsEqual func)
+{
+    int result = 0;
+    foreach (int i in numbers)
+    {
+        if (func(i))          // получаем значение i, сравнивает с условием внутри делегата и возвращает true/false
+        {
+            result += i;
+        }
+    }
+    return result;
+};
+#endregion
+
+#region Лямбда-выражение как результат метода
+//  Метод также может возвращать лямбда-выражение. В этом случае возвращаемым типом метода выступает делегат,
+//  которому соответствует возвращаемое лямбда-выражение.
+Operation2 SelectOperation(OperationType opType)
+{
+    switch (opType)
+    {
+        case OperationType.Add: return (x, y) => x + y;
+        case OperationType.Subtract: return (x, y) => x - y;
+        default: return (x, y) => x * y;
+    }
+}
+// Аналогичный вид записи
+Operation2 SelectOperation2(OperationType opType) => opType switch
+{
+    OperationType.Add => (x, y) => x + y, 
+    OperationType.Subtract => (x, y) => x + y, 
+    _ => (x, y) => x + y
+};
+#endregion
+
+#region Конец кода
+enum OperationType
+{
+    Add, Subtract, Multiply
+}
+delegate bool IsEqual(int x);
+delegate void Operation(int x, int y);
+delegate int Operation2(int x, int y);
+delegate void Message();
+delegate void PrintMessage(string message);
+#endregion
+
