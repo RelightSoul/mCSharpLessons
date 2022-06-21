ed9f20c55f1575334596419cf44e1cdddf9a19a7
RelightSoul <ajia9ikpobb@gmail.com>
2022-06-22 00:05:02 +0300

    Тема Pattern Matching

---
+++ 8.01.PatternTypes/Program.cs
@@ -0,0 +1,93 @@
+﻿//  Паттерн типов
+
+//  Pattern matching фактически выполняет сопоставление некоторого значения с некоторым шаблоном. И если
+//  сопоставление прошло успешно, то выполняются определенные действия. Язык C# позволяет выполнять различные
+//  типы сопоставлений.
+
+//  Паттерн типов или type pattern позволяет проверить некоторое значение на соответствие некоторому типу:
+
+//          значение is тип переменная_типа
+
+//  Например, у нас есть следующие классы:
+class Employee
+{
+    public virtual void Work() => Console.WriteLine("Employee works");
+}
+class Manager : Employee
+{
+    public bool IsOnVacation { get; set; }
+    public override void Work()
+    {
+        Console.WriteLine("Manager works");
+    }
+}
+
+//  С помощью паттерна типов проверим, представляет ли объект Employee класс Manager:
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee tom = new Manager();
+        UseEmployee(tom);
+        
+        void UseEmployee(Employee emp)
+        {
+            if (emp is Manager manager && manager.IsOnVacation == false)
+            {
+                manager.Work();
+            }
+            else
+            {
+                Console.WriteLine("Передаваемый не опознан");
+            }
+        }
+        // Здесь в методе UseEmployee значение emp сопоставляется с типом Manager. То есть в данном случае
+        // в качестве шаблона выступает тип Manager. Если сопоставление прошло успешно (то есть значение
+        // emp представляет тип Manager), в переменной manager оказывается объект emp. И далее мы можем
+        // вызвать у него методы и свойства.
+
+        //  Также мы можем использовать constant pattern - сопоставление с некоторой константой:
+        var message = "Hello";
+        if (message is "Hello")
+        {
+            Console.WriteLine("Привет");
+        }
+
+        //  Подобным образом, например, можно проверить значение на null:
+        Employee? bob = new Employee();
+        Employee? maks = null;
+        UseEmployee2(bob);
+        UseEmployee2(maks);
+
+        void UseEmployee2(Employee emp)
+        {
+            if (emp is not null)
+            {
+                emp.Work();
+            }
+        }
+
+        //  Кроме конструкции if сопоставление паттернов может применяться в конструкции switch:
+        UseEmployee3(tom);
+        UseEmployee3(bob);
+        UseEmployee3(maks);
+
+        void UseEmployee3(Employee emp)
+        {
+            switch (emp)
+            {
+                case Manager manager:
+                    manager.Work();
+                    Console.WriteLine("Manager works");
+                    break;
+                case null:
+                    Console.WriteLine("Object is null");
+                    break;
+                default:
+                    Console.WriteLine("Object not a manager");
+                    break;
+            }
+        }
+    }
+}
+
