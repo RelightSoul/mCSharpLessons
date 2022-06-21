f4a0a2eeca1bcbd925d934f25c58a2130af93a66
RelightSoul <ajia9ikpobb@gmail.com>
2022-06-21 15:03:57 +0300

    знакомые темы, легко разбираются

---
+++ 7.06.PartialClassesMethods/PersonAdditional.cs
@@ -0,0 +1,12 @@
+﻿public partial class Person
+{
+    partial void Check();
+    public partial void Read()
+    {
+        Console.WriteLine("Читает");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("I am eating");
+    }
+}
---
+++ 7.06.PartialClassesMethods/PersonBase.cs
@@ -0,0 +1,9 @@
+﻿public partial class Person
+{
+    partial void Check() { }
+    public partial void Read();
+    public void Move()
+    {
+        Console.WriteLine("I am moving");
+    }
+}
