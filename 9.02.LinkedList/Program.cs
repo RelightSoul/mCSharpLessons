a9b62c824c6d93a77191096b460e2e9ffde905f2
RelightSoul <ajia9ikpobb@gmail.com>
2022-06-23 19:22:53 +0300

    продолжаю тему Коллекций

--- 9.02.LinkedList/Program.cs
+++ 9.02.LinkedList/Program.cs
@@ -4,5 +4,104 @@
 //  на следующий и на предыдущий элемент.
 
 #region Создание связанного списка
+//  Для создания связного списка можно принименять один из его конструктора. Например, создадим пустой
+//  связный список:
+//  LinkedList<string> people = new LinkedList<string>();
 
+//  Также можно в конструктор передать коллекцию элементов, например, список List, по которому будет создан
+//  связный список:
+var employers = new List<string> { "Tom","Sam","Bob"};
+
+LinkedList<string> people = new LinkedList<string>(employers);
+#endregion
+
+#region LinkedListNode
+//  Если в простом списке List<T> каждый элемент представляет объект типа T, то в LinkedList<T> каждый узел
+//  представляет объект класса LinkedListNode<T>. А добавляемые в связанный список элементы T фактически
+//  обертываются в объект LinkedListNode.
+
+// Класс LinkedListNode имеет следующие свойства:
+// Value: возвращает или устанавливает само значение узла, представленное типом T
+// Next: возвращает ссылку на следующий элемент типа LinkedListNode<T> в списке. Если следующий элемент
+// отсутствует, то имеет значение null
+// Previous: возвращает ссылку предыдущий элемент типа LinkedListNode<T> в списке. Если предыдущий элемент
+// отсутствует, то имеет значение null
+#endregion
+
+#region Свойства LinkedList
+//  Класс LinkedList определяет следующие свойства:
+//  Count: количество элементов в связанном списке
+//  First: первый узел в списке в виде объекта LinkedListNode<T>
+//  Last: последний узел в списке в виде объекта LinkedListNode<T>
+
+Console.WriteLine(people.Count);         // 3
+Console.WriteLine(people.First?.Value);  // Tom
+Console.WriteLine(people.Last?.Value);   // Bob
+
+//  Используя свойства LinkedList и LinkedListNode, можно пройтись по всем элементам списка в прямом или
+//  обратном порядке:
+var curNodeIsFirst = people.First;
+while (curNodeIsFirst != null)
+{
+    Console.Write("{0}\t", curNodeIsFirst.Value);
+    curNodeIsFirst = curNodeIsFirst.Next;
+}
+Console.WriteLine();
+var curNodeIsLast = people.Last;
+while (curNodeIsLast != null)
+{
+    Console.Write("{0}\t", curNodeIsLast.Value);
+    curNodeIsLast = curNodeIsLast.Previous;
+}
+Console.WriteLine();
+#endregion
+
+#region Методы LinkedList
+//  Используя методы класса LinkedList<T>, можно обращаться к различным элементам, как в конце, так и в
+//  начале списка:
+
+//AddAfter(LinkedListNode < T > node, LinkedListNode < T > newNode): вставляет узел newNode в список после узла node.
+
+//AddAfter(LinkedListNode<T> node, T value): вставляет в список новый узел со значением value после узла node.
+
+//AddBefore(LinkedListNode<T> node, LinkedListNode<T> newNode): вставляет в список узел newNode перед узлом node.
+
+//AddBefore(LinkedListNode<T> node, T value): вставляет в список новый узел со значением value перед узлом node.
+
+//AddFirst(LinkedListNode<T> node): вставляет новый узел в начало списка
+
+//AddFirst(T value): вставляет новый узел со значением value в начало списка
+
+//AddLast(LinkedListNode<T> node): вставляет новый узел в конец списка
+
+//AddLast(T value): вставляет новый узел со значением value в конец списка
+
+//RemoveFirst(): удаляет первый узел из списка. После этого новым первым узлом становится узел, следующий за удаленным
+
+//RemoveLast(): удаляет последний узел из списка
+
+var _people = new LinkedList<string>();
+_people.AddLast("Tom"); // вставляем узел со значением Tom на последнее место
+                         //так как в списке нет узлов, то последнее будет также и первым
+_people.AddFirst("Bob"); // вставляем узел со значением Bob на первое место
+
+if (_people.First != null )
+{
+    _people.AddAfter(_people.First, "Mike");
+}
+/// вставляем после первого узла новый узел со значением Mike
+
+var company = new LinkedList<Person>();
+company.AddFirst(new Person {Name = "Paul" });
+company.AddLast(new Person {Name = "Rail" });
+company.AddAfter(company?.First, new Person { Name = "Kristina" });
+foreach (Person p in company)
+{
+    Console.Write("{0}\t",p.Name);
+}
+
+class Person
+{
+    public string Name { get; set; }
+}
 #endregion
---
+++ 9.03.Queue/Program.cs
@@ -0,0 +1,92 @@
+﻿// Класс Queue<T> представляет обычную очередь, которая работает по алгоритму FIFO ("первый вошел - первый вышел").
+
+#region Создание очереди
+//  Для создания очереди можно использовать один из трех ее конструкторов.
+//  Прежде всего можно создать пустую очередь:
+Queue<string> people = new Queue<string>();
+//  При создании пустой очереди можно указать емкость очереди:
+Queue<string> people2 = new Queue<string>(16);
+
+//  Также можно инициализировать очередь элементами из другой коллекции или массивом:
+var employees = new List<string> { "Tom", "Sam", "Bob" };
+Queue<string> people3 = new Queue<string>(employees);
+//Для перебора очереди можно использовать стандартный цикл foreach.
+//Для получения количества элементов в очереди в классе определено свойство Count.
+#endregion
+
+#region Методы Queue
+//  У класса Queue<T> можно отметить следующие методы:
+//  void Clear(): очищает очередь
+//  bool Contains(T item): возвращает true, если элемент item имеется в очереди
+//  T Dequeue(): извлекает и возвращает первый элемент очереди
+//  void Enqueue(T item): добавляет элемент в конец очереди
+//  T Peek(): просто возвращает первый элемент из начала очереди без его удаления
+
+var _people = new Queue<string>();
+
+// добавляем элементы
+_people.Enqueue("Tom"); // people = { Tom }
+_people.Enqueue("Sam"); // people = { Tom, Bob }
+_people.Enqueue("Bob"); // people = { Tom, Bob, Sam }
+
+// получаем элемент из начала очереди 
+var fPerson = _people.Peek();
+
+// удаляем элементы
+var p1 = _people.Dequeue(); // people = { Bob, Sam }
+Console.WriteLine(p1);      // Tom
+//  Стоит отметить, что если с помощью методов Peek или Enqueue мы попытаемся получить первый элемент очереди,
+//  которая пуста, то программа выдаст исключение. Соответственно перед получением элемента мы можем проверять
+//  количество элементов в очереди:
+
+//Либо можно использовать пару методов:
+//bool TryDequeue(out T result): передает в переменную result первый элемент очереди с его удалением из очереди,
+//возвращает true, если очередь не пуста и элемент успешно получен.
+//bool TryPeek(out T result): передает в переменную result первый элемент очереди без его извлечения из очереди,
+//возвращает true, если очередь не пуста и элемент успешно получен.
+
+var _people1 = new Queue<string>();
+_people1.Enqueue("Tom");
+
+var success1 = _people1.TryDequeue(out var pers1);   // success1 = true
+if (success1) Console.WriteLine(pers1);              // Tom
+
+var success2 = people.TryPeek(out var pers2);  // success2 = false
+if (success2) Console.WriteLine(pers2);
+#endregion
+
+//  Очереди - довольно часто встречаемая стуктура в реальной жизни. Например, очередь пациентов на прием к врачу.
+//  Реализуем данную ситуацию:
+
+var patients = new Queue<Person>();
+patients.Enqueue(new Person("Kara"));
+patients.Enqueue(new Person("German"));
+patients.Enqueue(new Person("Elis"));
+
+var doctor = new Doctor();
+doctor.TakePatients(patients);
+
+class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;    
+    }
+}
+class Doctor
+{
+    public void TakePatients(Queue<Person> que)
+    {
+        while (que.Count > 0 )
+        {
+            var patient = que.Dequeue();
+            Console.WriteLine($"Осмотр пациента {patient.Name}");
+        }
+        Console.WriteLine("Осмотр завершён");
+    }
+}
+//  Здесь класс врача - класс Doctor в методе TakePatients принимает очередь пациентов в виде объектов Person.
+//  И пока в очереди есть объекты извлекает по одному объекту. Консольный вывод:
+
+
