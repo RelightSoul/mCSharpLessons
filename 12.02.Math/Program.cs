acca075770cb3723e54b2e3fdad759320515670e
RelightSoul <ajia9ikpobb@gmail.com>
2022-06-24 23:42:00 +0300

    Сладкие темы, куча сахара, большую часть уже использовал

---
+++ 12.02.Math/Program.cs
@@ -0,0 +1,87 @@
+﻿// Математические вычисления и класс Math
+
+//  Для выполнения различных математических операций в библиотеке классов .NET предназначен класс Math.
+//  Он является статическим, поэтому все его методы также являются статическими.
+
+//  Рассмотрим основные методы класса Math:
+
+//  Abs(double value): возвращает абсолютное значение для аргумента value
+double result = Math.Abs(-12.4); // 12.4
+
+//  Acos(double value): возвращает арккосинус value. Параметр value должен иметь значение от -1 до 1
+double result2 = Math.Acos(1); // 0
+
+//Asin(double value): возвращает арксинус value. Параметр value должен иметь значение от -1 до 1
+
+//Atan(double value): возвращает арктангенс value
+
+//BigMul(int x, int y): возвращает произведение x * y в виде объекта long
+double result3 = Math.BigMul(100, 9340); // 934000
+
+//Ceiling(double value): возвращает наименьшее целое число с плавающей точкой, которое не меньше value
+double result4 = Math.Ceiling(2.34); // 3
+
+//Cos(double d): возвращает косинус угла d
+
+//Cosh(double d): возвращает гиперболический косинус угла d
+
+//DivRem(int a, int b, out int result): возвращает результат от деления a/b, а остаток помещается в параметр
+//result
+int result5;
+int div = Math.DivRem(14, 5, out result5);
+// result5 = 4
+// div = 2
+
+//Exp(double d): возвращает основание натурального логарифма, возведенное в степень d
+
+//Floor(decimal d): возвращает наибольшее целое число, которое не больше d
+double result6 = Math.Floor(2.56); // 2
+
+//IEEERemainder(double a, double b): возвращает остаток от деления a на b
+double result7 = Math.IEEERemainder(26, 4); // 2 = 26-24
+
+//Log(double d): возвращает натуральный логарифм числа d
+
+//Log(double a, double newBase): возвращает логарифм числа a по основанию newBase
+
+//Log10(double d): возвращает десятичный логарифм числа d
+
+//Max(double a, double b): возвращает максимальное число из a и b
+
+//Min(double a, double b): возвращает минимальное число из a и b
+
+//Pow(double a, double b): возвращает число a, возведенное в степень b
+
+//Round(double d): возвращает число d, округленное до ближайшего целого числа
+double result8 = Math.Round(20.56); // 21
+double result9 = Math.Round(20.46); //20
+
+//Round(double a, int b): возвращает число a, округленное до определенного количества знаков после запятой,
+//представленного параметром b
+double result10 = Math.Round(20.567, 2); // 20,57
+double result11 = Math.Round(20.463, 1); //20,5
+
+//Sign(double value): возвращает число 1, если число value положительное, и -1, если значение value отрицательное.
+//Если value равно 0, то возвращает 0
+int result12 = Math.Sign(15); // 1
+int result13 = Math.Sign(-5); //-1
+
+//Sin(double value): возвращает синус угла value
+
+//Sinh(double value): возвращает гиперболический синус угла value
+
+//Sqrt(double value): возвращает квадратный корень числа value
+double result14 = Math.Sqrt(16); // 4
+
+//Tan(double value): возвращает тангенс угла value
+
+//Tanh(double value): возвращает гиперболический тангенс угла value
+
+//Truncate(double value): отбрасывает дробную часть числа value, возвращаяя лишь целое значние
+double result15 = Math.Truncate(16.89); // 16
+
+//Также класс Math определяет две константы: Math.E и Math.PI. Например, вычислим площадь круга:
+double radius = 50;
+double area = Math.PI * Math.Pow(radius, 2);
+Console.WriteLine($"Площадь круга с радиусом {radius} равна {Math.Round(area, 2)}");
+//Площадь круга с радиусом 50 равна 7853,98
---
+++ 12.03.Convert/Program.cs
@@ -0,0 +1,77 @@
+﻿// Преобразование типов и класс Convert
+
+#region Методы Parse и TryParse
+//  Все примитивные типы имеют два метода, которые позволяют преобразовать строку к данному типу.
+//  Это методы Parse() и TryParse().
+
+//  Метод Parse() в качестве параметра принимает строку и возвращает объект текущего типа. Например:
+using System.Globalization;
+
+int a = int.Parse("34");
+double d = double.Parse("334,3");
+decimal c = decimal.Parse("12,45");
+byte b = byte.Parse("4");
+//  Стоит отметить, что парсинг дробных чисел зависит от настроек текущей культуры. В частности, для
+//  получения числа double я передаю строку "23,56" с запятой в качестве разделителя. Если бы я передал
+//  точку вместо запятой, то приложение выдало ошибку выполнения. На компьютерах с другой локалью,
+//  наоборот, использование запятой вместо точки выдало бы ошибку.
+
+//  Чтобы не зависеть от культурных различий мы можем установить четкий формат с помощью класса
+//  NumberFormatInfo и его свойства NumberDecimalSeparator:
+
+IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+double d2 = double.Parse("334.3", formatter);
+double d3 = double.Parse("334,3");
+Console.WriteLine(d2 + " = " + d3);
+
+//  В данном случае в качестве разделителя устанавливается точка. Однако тем не менее потенциально при
+//  использовании метода Parse мы можем столкнуться с ошибкой, например, при передачи алфавитных символов
+//  вместо числовых. И в этом случае более удачным выбором будет применение метода TryParse(). Он пытается
+//  преобразовать строку к типу и, если преобразование прошло успешно, то возвращает true. Иначе возвращается
+//  false:
+
+Console.WriteLine();
+Console.WriteLine("Введите строку: ");
+string? input = Console.ReadLine();
+
+bool result = int.TryParse(input, out a);
+if (result == true)
+{
+    Console.WriteLine(a);
+}
+else if (result == false)
+{
+    Console.WriteLine("Неудачно");
+}
+//  Если преобразование пройдет неудачно, то исключения никакого не будет выброшено, просто метод TryParse
+//  возвратит false, а переменная number будет содержать значение по умолчанию.
+#endregion
+
+#region Convert
+//Класс Convert представляет еще один способ для преобразования значений. Для этого в нем определены
+//следующие статические методы:
+
+//ToBoolean(value)
+//ToByte(value)
+//ToChar(value)
+//ToDateTime(value)
+//ToDecimal(value)
+//ToDouble(value)
+//ToInt16(value)
+//ToInt32(value)
+//ToInt64(value)
+//ToSByte(value)
+//ToSingle(value)
+//ToUInt16(value)
+//ToUInt32(value)
+//ToUInt64(value)
+
+//  В качестве параметра в эти методы может передаваться значение различных примитивных типов,
+//  необязательно строки:
+int n = Convert.ToInt32("345");
+double doub = Convert.ToDouble(true);
+Console.WriteLine(n +" "+ doub);
+
+//  Однако опять же, как и в случае с методом Parse, если методу не удастся преобразовать значение к нужному
+//  типу, то он выбрасывает исключение FormatException.
+#endregion
